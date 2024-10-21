6a4ddb3959eecd0d69479455778837e15dc983a5
Tom <tom.makni@gmail.com>
2024-10-21 10:25:43 +0000

    ok

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -2,8 +2,12 @@
 
 
 int	main(void)
-
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 {
-	puts("Programming is like building a multilingual puzzle);
+	puts("Programming is like building a multilingual puzzle");
 	return (0);
 }
