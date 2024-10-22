ae7d96cc0ea9ee7ac963cbeac22bbc73ebb02628
tom <tom.makni@gmail.com>
2024-10-22 13:39:02 +0000

    ok

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+/* more headers goes there */
+
 /**
- * main - Determines if a number is positive, negative or zero.
+ * main - Entry point
+ *
  *
  * Return: Always 0 (Success)
  */
