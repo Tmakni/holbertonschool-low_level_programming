365d8f87687a2d801fe8dcb3aaaf02aa4825705f
tom <tom.makni@gmail.com>
2024-10-22 08:58:03 +0000

    ok

---
+++ variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int	main ()
+{
+	int n;
+
+	if (n == 0)
+	{
+		puts("is zero");
+	}
+
+	else if (n > 0)
+	{
+		puts("is positive");
+	}
+
+	else
+	{
+		puts("is negative");
+	}
+
+	return(0);
+}
