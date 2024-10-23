8cbde32cdffaa899465d362b396e061ec460bec3
tom <tom.makni@gmail.com>
2024-10-23 07:59:43 +0000

    ok

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -1,14 +1,12 @@
 #include <unistd.h>
 #include <stdio.h>
-
 /**
  * main - Prints the alphabetic
  * Return: Always (Success)
  */
-
 int main(void)
 {
 	write(1, "abcdefghijklmnopqrstuvwxyz\n", 27);
-	write(1, "ABCDEFGHIJKLMNOPQRSTUVWXYZ", 26);
-	return(0);
+	write(1, "ABCDEFGHIJKLMNOPQRSTUVWXYZ\n", 27);
+	return (0);
 }
