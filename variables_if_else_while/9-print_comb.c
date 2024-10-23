7ed89b17e0f79968a6039daeedb8fe89d601e202
tom <tom.makni@gmail.com>
2024-10-23 08:34:12 +0000

    ok

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,16 @@
+#include <unistd.h>
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabetic
+ * Return: Always (Success)
+ */
+
+int main(void)
+
+{
+
+	write(1, "0, 1, 2, 3, 4, 5, 6, 7, 8, 9\n", 29);
+	return (0);
+
+}
