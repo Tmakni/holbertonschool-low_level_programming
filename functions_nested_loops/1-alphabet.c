e76853d5dfefc0f451ea4a99f593623faf7affe4
tom <tom.makni@gmail.com>
2024-10-23 12:16:37 +0000

    ok

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 /**
  * main - check the code
  *
@@ -6,6 +7,6 @@
  */
 int main(void)
 {
-	write(1, "abcdefghijklmnopqrstuvwxyz\n", 27)
+	printf("abcdefghijklmnopqrstuvwxyz\n");
 	return (0);
 }
