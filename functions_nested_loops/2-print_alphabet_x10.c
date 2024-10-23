038bad8c1ea36645dc94ae99abbbaa4c641fe0ad
tom <tom.makni@gmail.com>
2024-10-23 14:04:30 +0000

    ok

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,15 +1,15 @@
 #include <unistd.h>
 /**
- * main - check the code.
- *
- * Return: Always 0.
+ * print_alphabet_x10 - Entry point
+ * Return: 0
  */
 void print_alphabet_x10(void)
+
 {
 	int i;
 
 	i = 0;
-	while(i != 10)
+	while (i != 10)
 	{
 		write(1, "abcdefghijklmnopqrstuvwxyz\n", 27);
 		i++;
