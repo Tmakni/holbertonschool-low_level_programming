6f424b366829169709dfc1e2eadc1e50508d0437
tom <tom.makni@gmail.com>
2024-10-24 13:58:12 +0000

    ok

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- *  _islower - Entry point
+ *  _isalpha - Entry point
  *  @c : input
  * Return: 0
  */
 int _isalpha(int c)
 {
-	if (c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z')
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 		return (1);
 	else
 		return (0);
