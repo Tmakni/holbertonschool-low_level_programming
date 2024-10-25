59166a8c79f123d589c7f72a78d0d338153f865a
tom <tom.makni@gmail.com>
2024-10-25 07:33:15 +0000

    ok

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_last_digit - check the code
+ * @n : input
+ *
+ * Return: Always 0.
+ */
+int print_last_digit(int n)
+{
+	if (n < 0)
+		n = n * -1;
+	_putchar((n % 10) + '0');
+	return (n % 10);
+}
