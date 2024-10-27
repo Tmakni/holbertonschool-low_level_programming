79690aea0b133d6a6b050b5f370d3d091e4e131b
tom <tom.makni@gmail.com>
2024-10-27 13:51:19 +0000

    ok

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+* add - fonction
+*
+* @i: input
+* @j: input
+*
+* Return: add
+*/
+
+int add(int i, int j);
+{
+	return (i, j);
+}
--- functions_nested_loops/main.c
+++ functions_nested_loops/main.c
@@ -1,5 +1,8 @@
 int main(void)
 {
-    times_table();
+    int n;
+
+    n = add(89, 9);
+    printf("%d\n", n);
     return (0);
 }
