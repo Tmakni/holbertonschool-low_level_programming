9e234113e92e92e3a77483ff0c6a20f02660ece3
tom <tom.makni@gmail.com>
2024-10-28 12:16:35 +0000

    ok

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <unistd.h>
+/**
+ * print_line - The shortest distance between two points is a straight line
+ * Description: print _
+ * @n: input
+ * Return: 0
+ */
+void print_line(int n)
+{
+	int i;
+	if (n > 0)
+	while (i = '0' && i < n)
+	{
+		_putchar('_');
+		_putchar('\n');
+		i++;
+	}
+}
