55bce696a6bf162c92caed29316a5088194e9f99
tom <tom.makni@gmail.com>
2024-10-28 10:58:28 +0000

    ok

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <unistd.h>
+/**
+ * print_most_numbers - I believe in numbers and signs
+ * Description: print 0 a 9
+ * Return: rien
+ */
+void print_most_numbers(void)
+{
+	write(1, "1356789\n", 8);
+}
