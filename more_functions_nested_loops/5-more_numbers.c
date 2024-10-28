f7af81eed260885d422af38498cf88e4679b5b3b
tom <tom.makni@gmail.com>
2024-10-28 11:08:05 +0000

    ok

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -1,5 +1,10 @@
 #include "main.h"
-
+#include <unistd.h>
+/**
+ * more_numbers - Numbers constitute the only universal language
+ * Description: print 0 a 9 * 10
+ * Return: rien
+ */
 void more_numbers(void)
 {
 	int i = '0';
