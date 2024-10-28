ecc7921bfb229f3b4c48a589ec75ee7d4ba6fe88
tom <tom.makni@gmail.com>
2024-10-28 09:01:01 +0000

    ok

--- more_functions_nested_loops/1-isdigit.c
+++ more_functions_nested_loops/1-isdigit.c
@@ -1,13 +1,11 @@
 #include "main.h"
 /**
- * _isdigit - check the code
- * @n: compteur
+ * _isdigit - c
+ * @c: compteur
  * Return: c
  */
 int _isdigit(int c)
 {
-	if (c >= 0 && c <= 9)
+	if (c >= 48 && c <= 57)
 		return (1);
-	else
-		return (0);
 }
