7be78d6a10dfc13f492a21592246460ea355d6b5
tom <tom.makni@gmail.com>
2024-10-28 08:52:45 +0000

    ok

--- more_functions_nested_loops/main.c
+++ more_functions_nested_loops/main.c
@@ -1,12 +1,11 @@
 #include "main.h"
-
 int main(void)
 {
     char c;
 
-    c = 'A';
-    printf("%c: %d\n", c, _isupper(c));
+    c = '0';
+    printf("%c: %d\n", c, _isdigit(c));
     c = 'a';
-    printf("%c: %d\n", c, _isupper(c));
+    printf("%c: %d\n", c, _isdigit(c));
     return (0);
 }
