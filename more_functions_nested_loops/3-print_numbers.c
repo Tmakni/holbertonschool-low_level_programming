a48363d8cde8982605bc3bc04d07c1627e5c2ea9
tom <tom.makni@gmail.com>
2024-10-28 09:42:28 +0000

    ok

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <unistd.h>
+/**
+ * print_numbers - check the code
+ * @i : input
+ * Return: Always 0.
+ */
+void print_numbers(void)
+{
+int i;
+
+i = 0;
+	while (i >= 0 && i <= 9)
+	{
+		write(1, &i, 1);
+		write(1, "\n", 1);
+		i++;
+	}
+}
