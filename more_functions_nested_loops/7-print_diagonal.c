9ccf961cab9f8b0c5d7b7b819b623c686877a278
tom <tom.makni@gmail.com>
2024-10-29 10:35:21 +0000

    ok

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -1,6 +1,5 @@
 #include "main.h"
 #include <unistd.h>
-#include <stdio.h>
 /**
  * print_diagonal - check the code
  * @n: compteur
@@ -8,9 +7,8 @@
  */
 void print_diagonal(int n)
 {
-	int i;
+	int i = 0;
 
-	i = 0;
 	if (n < 0)
 	{
 	write(1, "\n", 1);
@@ -18,22 +16,16 @@ void print_diagonal(int n)
 		while (i < n)
 		{
 			int j = 0;
+
 			while (j < i)
 			{
 				write(1, " ", 1);
 				j++;
+
 			}
+
 			write(1, "\\", 1);
 			write(1, "\n", 1);
 			i++;
 		}
-}
-
-int main(void)
-{
-    print_diagonal(0);
-    print_diagonal(2);
-    print_diagonal(10);
-    print_diagonal(-4);
-    return (0);
 }
