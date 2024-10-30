2752307d8c40c61e098130093a9c0c2050bf9a36
tom <tom.makni@gmail.com>
2024-10-30 09:06:52 +0000

    ok

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -8,24 +8,30 @@ void FizzBuzz(int i)
 
 	while (i <= 100)
 	{
+
 	if (i % 15 == 0)
 	{
-		printf(" FizzBuzz ");
+		printf("FizzBuzz");
 	}
 	else if (i % 3 == 0)
 	{
-		printf(" Fizz ");
+		printf("Fizz");
 	}
 	else if (i % 5 == 0)
 	{
-		printf(" Buzz ");
+		printf("Buzz");
 	}
 	else
 	{
-		printf("%d\n", i);
+		printf("%d", i);
+	}
+	if (i < 100)
+	{
+		printf(" ");
 	}
 	i++;
 	}
+	printf("\n");
 }
 int main()
 {
