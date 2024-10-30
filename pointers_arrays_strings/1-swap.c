fed93df13496e2b4c0bcc3dc026fb0b9714bea17
tom <tom.makni@gmail.com>
2024-10-30 10:54:28 +0000

    ok

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * swap_int - Set pointer 98, 42
+ * @a: int type pointer
+ * @b: int type pointer
+ */
+void swap_int(int *a, int *b)
+{
+	*a = 98;
+	*b = 42;
+}
