92cedacbf79d79bed41891d7eb6d0e22225bb88e
tom <tom.makni@gmail.com>
2024-10-30 11:25:23 +0000

    ok

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * _strlen - return the length of the string
+ * @s: int type pointer
+ * Return: length of string
+ */
+int _strlen(char *s)
+{
+	char i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
