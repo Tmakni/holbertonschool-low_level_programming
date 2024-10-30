cd243ed6ee99d7de4a880fc429cf0d1afbc0d5d7
tom <tom.makni@gmail.com>
2024-10-30 12:24:35 +0000

    ok

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * rev_string - string reverse
+ * @s: caractere
+ */
+void rev_string(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	while (i > 0)
+	{
+		i--;
+	}
+}
