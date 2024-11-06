4068a9acdea95a26df6a136b54c764b2609b30e6
tom <tom.makni@gmail.com>
2024-11-06 09:29:29 +0000

    ok

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -3,19 +3,18 @@
  * @s: sting
  * @b: acrementation
  * @n: decompteur
- *
+ * Description: fill memory poiteur string
  * Return: 1
  */
 
 char *_memset(char *s, char b, unsigned int n)
 {
-    unsigned int i = 0;
+	unsigned int i = 0;
 
-    while (i < n)
-    {
-        s[i] = b;
-        i++;
-    }
-
-    return s;
+	while (i < n)
+	{
+		s[i] = b;
+		i++;
+	}
+	return (s);
 }
