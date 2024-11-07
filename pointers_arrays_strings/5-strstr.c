3f43e66d8732a240b72e049a74d79052e87a2bcb
tom <you@example.com>
2024-11-07 10:20:35 +0000

    ok

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -13,10 +13,10 @@ char *_strstr(char *haystack, char *needle)
 	if (haystack[i] == needle[j])
 		return (0);
 
-	while(haystack[i] != '\0')
+	while (haystack[i] != '\0')
 	{
 		j = 0;
-		while(needle[j] != '\0')
+		while (needle[j] != '\0')
 		{
 			if (haystack[i] == needle[j])
 			{
