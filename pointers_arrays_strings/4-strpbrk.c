216bdde7c85d592fcce31d33db2aea4417738870
tom <you@example.com>
2024-11-07 10:17:07 +0000

    ok

--- pointers_arrays_strings/4-strpbrk.c
+++ pointers_arrays_strings/4-strpbrk.c
@@ -14,7 +14,7 @@ char *_strpbrk(char *s, char *accept)
 	while (s[i] != '\0')
 	{
 		j = 0;
-		while (s[j] != '\0')
+		while (accept[j] != '\0')
 		{
 			if (s[i] == accept[j])
 			{
---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+char *_strstr(char *haystack, char *needle)
+{
+	int i = 0;
+	int j;
+
+	if (haystack[i] == needle[j])
+		return (0);
+
+	while(haystack[i] != '\0')
+	{
+		j = 0;
+		while(needle[j] != '\0')
+		{
+			if (haystack[i] == needle[j])
+			{
+				i++;
+			}
+			else
+			{
+				j++;
+				break;
+			}
+			return (haystack + i - 1);
+		}
+		i++;
+	}
+	return (0);
+}
+
+int main(void)
+{
+    char *s = "hello, world";
+    char *f = "world";
+    char *t;
+
+    t = _strstr(s, f);
+    printf("%s\n", t);
+    return (0);
+}
