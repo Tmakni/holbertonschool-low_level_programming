368680556df39c8b88b3741a93e69690f336b259
tom <you@example.com>
2024-11-07 08:57:05 +0000

    ok

---
+++ pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,28 @@
+/**
+ * _strspn - file memory
+ * @s: string
+ * @accept: string
+ * Desc: print memory s
+ * Return: 1
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int i = 0;
+	int j;
+	int compt = 0;
+
+	while (s[i] != '\0')
+	{
+		j = 0;
+		while (s[j] != '\0')
+		{
+			if (s[i] == accept[j])
+			{
+				return (s + compt);
+			}
+			j++;
+		}
+		compt++;
+		i++;
+	}
+}
