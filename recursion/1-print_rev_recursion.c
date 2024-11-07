c1cd8b43a5b8aaa255488c5d9b4549c73e0096ea
tom <you@example.com>
2024-11-07 11:29:08 +0000

    ok

--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -9,11 +9,10 @@ void _print_rev_recursion(char *s)
 {
 	int i = 0;
 
-	if (s[i] < '\0')
+	if (s[i] == '\0')
 	{
-		write(1, &s[i], 1);
 		return;
 	}
+	_print_rev_recursion(s + 1);
 	write(1, &s[i], 1);
-	_print_rev_recursion(s - 1);
 }
