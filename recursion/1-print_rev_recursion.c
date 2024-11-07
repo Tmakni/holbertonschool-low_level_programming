61146bc425236f12544351361b5b666841f6210b
tom <you@example.com>
2024-11-07 11:03:18 +0000

    ok

---
+++ recursion/1-print_rev_recursion.c
@@ -0,0 +1,19 @@
+#include <unistd.h>
+/**
+ * _puts_recursion - print recursion
+ *@s: string
+ *Desc: recursion print
+ * Return: 1
+ */
+void _print_rev_recursion(char *s)
+{
+	int i = 0;
+
+	if (s[i] == '\0')
+	{
+		write(1, "\n", 1);
+		return;
+	}
+	write(1, &s[i], 1);
+	_print_rev_recursion(s - 1);
+}
