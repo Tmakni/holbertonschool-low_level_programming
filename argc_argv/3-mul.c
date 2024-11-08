369364a100464e1097240815406da788138e26cf
Your Name <you@example.com>
2024-11-08 14:59:28 +0000

    ok

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+/**
+ * main - *
+ * @argc: argument
+ * @argv: argument
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	return (0);
+}
