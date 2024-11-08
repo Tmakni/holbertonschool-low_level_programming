523e08ac80e173ed2ecb8b4f93a1e21d0816aa19
Your Name <you@example.com>
2024-11-08 14:29:58 +0000

    ok

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+/**
+ * main - print num
+ * @argc: argument 
+ * @argv: argument 
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argv;
+
+	printf("%d\n", argc - 1);
+
+	return (0);
+}
