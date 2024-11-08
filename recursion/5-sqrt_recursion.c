e1a75eedfdf42d17a5a70bac89f557a6ba562ba2
Your Name <you@example.com>
2024-11-08 12:30:27 +0000

    ok

---
+++ recursion/5-sqrt_recursion.c
@@ -0,0 +1,18 @@
+/**
+* _sqrt_recursion - racine
+ * @n: int number
+ * Return 1
+ */
+int _sqrt_recursion(int n)
+{
+	int i;
+	if (i == n)
+		return (i);
+	else if (i < n)
+	{
+		return (n + 1);
+	}
+	else
+		return (-1);
+
+}
