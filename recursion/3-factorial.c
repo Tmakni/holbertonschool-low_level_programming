ce135df17c1adc5e8a496343af134f7a8304647c
Your Name <you@example.com>
2024-11-08 10:25:05 +0000

    ok

--- recursion/3-factorial.c
+++ recursion/3-factorial.c
@@ -1,31 +1,15 @@
-#include <stdio.h>
 int factorial(int n)
 {
-	if (n == 0)
-	{
-		return (1);
-	}
 	if (n < 0)
 	{
-		return(-1);
+		return (-1);
 	}
-	if (n > 0)
+	else if (n < 2)
 	{
-		return(n + factorial(n - 1));
-		n--;
+		return(n);
 	}
-}
-int main(void)
-{
-    int r;
-
-    r = factorial(1);
-    printf("%d\n", r);
-    r = factorial(5);
-    printf("%d\n", r);
-    r = factorial(10);
-    printf("%d\n", r);
-    r = factorial(-1024);
-    printf("%d\n", r);
-    return (0);
+	else
+	{
+		return(n * factorial(n - 1));
+	}
 }
