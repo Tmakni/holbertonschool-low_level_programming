863a3262e8afc55f66e9116338f71eae26f6a777
Your Name <you@example.com>
2024-11-08 14:46:00 +0000

    ok

---
+++ argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - print
+ * @argc: argument
+ * @argv: arguments
+ * Return: 0
+ */
+int main(int argc, char *argv)
+{
+	(void) argc;
+	printf("%d\n", argv[-1]);
+	return (0);
+
+}
