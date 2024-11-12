5f35ca233498baa309e2316cf48c725b6c33d6e7
Your Name <you@example.com>
2024-11-12 14:45:34 +0000

    ok

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -1,21 +1,61 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 char *create_array(unsigned int size, char c)
 {
 	int *str;
-	int i = 0;
-
-	while (str[i] != '\0')
+	unsigned int i;
+	
+	if (size == 0)
 	{
+		return (NULL);
+	}
+	str = malloc (sizeof (char) * size);
+	
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	while (i = 0 && i < size)
+	{
+		str[i] = c;
 		i++;
 	}
-	return (i);
 	
-	str = malloc(sizeof (char) * (*create_array));
-	if (size = 0)
-	{
-		return (0);
-	}
-	free (str);
+	free(str);
 	return (0);
+}
+void simple_print_buffer(char *buffer, unsigned int size)
+{
+    unsigned int i;
+
+    i = 0;
+    while (i < size)
+    {
+        if (i % 10)
+        {
+            printf(" ");
+        }
+        if (!(i % 10) && i)
+        {
+            printf("\n");
+        }
+        printf("0x%02x", buffer[i]);
+        i++;
+    }
+    printf("\n");
+}
+int main(void)
+{
+    char *buffer;
+
+    buffer = create_array(98, 'H');
+    if  (buffer == NULL)
+    {
+        printf("failed to allocate memory\n");
+        return (1);
+    }
+    simple_print_buffer(buffer, 98);
+    free(buffer);
+    return (0);
 }
