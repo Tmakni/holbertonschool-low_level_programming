056ff4a0cace481fa66d0238350756764b33d3c1
Your Name <you@example.com>
2024-11-13 09:10:45 +0000

    ok

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -1,23 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
+
 /**
- * *create_array: malloc
+ * *create_array - malloc
  * @size: taille
  * @c: tableu compteur
  * Desc: malloc
+ * Return: 1
  */
 char *create_array(unsigned int size, char c)
 {
 	char *str;
 	unsigned int i;
-	
+
 	if (size == 0)
 	{
 		return (0);
 	}
-	str = malloc (sizeof (char) * size);
-	
+	str = malloc(sizeof(char) * size);
+
 	if (str == 0)
 	{
 		return (0);
@@ -25,8 +26,8 @@ char *create_array(unsigned int size, char c)
 	i = 0;
 	while (i < size)
 	{
-			str[i] = c;
-			i++;
+		str[i] = c;
+		i++;
 	}
 	return (str);
 }
