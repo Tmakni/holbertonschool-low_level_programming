4c58b98e2330941402fb04bede3ad58fe9c70640
Your Name <you@example.com>
2024-11-13 13:49:06 +0000

    ok

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -1,9 +1,38 @@
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 char *str_concat(char *s1, char *s2)
 {
 	char *p;
-	unsigned int i = 0;
+	unsigned int i, j, len1, len2;
 
-	p = malloc(sizeof(char) * (i));
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 
+	len1 = strlen(s1);
+	len2 = strlen(s2);
+
+	p = malloc(sizeof(char) * (len1 + len2 + 1));
+	if (p == NULL)
+	{
+		return (NULL);
+	}
+	i = 0;
+	while (i < len1)
+	{
+		p[i] = s1[i];
+		i++;
+	}
+
+	j = 0;
+	while (j < len2)
+	{
+		p[i + j] = s2[j];
+		j++;
+	}
+	p[i + j] = '\0';
+
+	return (p);
 }
