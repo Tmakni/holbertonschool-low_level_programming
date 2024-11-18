bf103a81712d35657b12a5184a3aa30b1da4a01b
Your Name <you@example.com>
2024-11-18 09:04:11 +0000

    ok

---
+++ more_malloc_free/2-calloc.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * _calloc - allocates memory
+ * @nmemb: number
+ * @size: size
+ * Return: 1
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	int *p;
+	unsigned int i;
+
+	if (nmemb <= 0 || size <= 0)
+		return (NULL);
+
+	p = malloc(nmemb * size);
+	if (p == NULL)
+		return (NULL);
+	for (i = 0; i < nmemb; i++)
+		p[i] = 0;
+
+	return (p);
+}
