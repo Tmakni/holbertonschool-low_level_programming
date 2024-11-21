21df869549c2dc70c561ddeb3f382befc712f30e
Your Name <you@example.com>
2024-11-21 11:21:09 +0000

    ok

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -1,10 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
+/**
+ * sum_them_all - Adds all its parameters.
+ * @n: The number of parameters passed to the function.
+ *
+ * Return: 1
+ */
 int sum_them_all(const unsigned int n, ...)
 {
+	va_list args;
+	int sum = 0;
+	unsigned int i;
 	if (n == 0)
 	{
 		return (0);
 	}
+	va_start(args, n);
 
+	while (i < n)
+	{
+		sum += va_arg(args, int);
+		i++;
+	}
+	va_end(args);
+
+	return (sum);
 }
