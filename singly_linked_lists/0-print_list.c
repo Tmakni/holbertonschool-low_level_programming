721d4d2b1833c07e0bfe470494b77ca63b97e0d4
Your Name <you@example.com>
2024-12-04 15:15:25 +0000

    ok

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -1,17 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+
 size_t print_list(const list_t *h)
 {
-    size_t node_count = 0; // Initialisation du compteur
+	size_t node_count = 0; // Initialisation du compteur
 
-    while (h != NULL) // Tant qu'il y a des nœuds dans la liste
-    {
-        if (h->str == NULL) // Si la chaîne est NULL
-            printf("[0] (nil)\n");
-        else // Sinon, afficher la longueur et la chaîne
-            printf("[%u] %s\n", h->len, h->str);
+	while (h != NULL) // Tant qu'il y a des nœuds dans la liste
+	{
+		if (h->str == NULL) // Si la chaîne est NULL
+		printf("[0] (nil)\n");
+	else // Sinon, afficher la longueur et la chaîne
+		printf("[%u] %s\n", h->len, h->str);
 
-        node_count++; // Incrémenter le compteur de nœuds
-        h = h->next;  // Passer au prochain nœud
-    }
-
-    return node_count; // Retourner le nombre total de nœuds
+	node_count++; // Incrémenter le compteur de nœuds
+	h = h->next;  // Passer au prochain nœud
+	}
+	return node_count; // Retourner le nombre total de nœuds
 }
