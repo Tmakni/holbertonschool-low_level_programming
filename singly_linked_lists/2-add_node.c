fad1289c95a25ffae92ca2d603d8fb5432390a19
Your Name <you@example.com>
2024-12-05 10:01:07 +0000

    ok

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -1,3 +1,15 @@
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+
+/**
+ * add_node - Ajoute un nouveau nœud au début d'une liste chaînée
+ * @head: Double pointeur vers le premier nœud de la liste
+ * @str: Chaîne de caractères à dupliquer dans le nouveau nœud
+ *
+ * Return: Adresse du nouveau nœud ou NULL en cas d'échec
+ */
+
 list_t *add_node(list_t **head, const char *str)
 {
 	size_t count = 0;
