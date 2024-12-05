dbf8e8d6c4715ec3c9c70e1b9428874d61bb0803
Your Name <you@example.com>
2024-12-05 09:49:08 +0000

    ok

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -4,9 +4,10 @@
 #include "lists.h"
 
 /**
- * print_list - fonction
- * @h: heqd
- * Return: 1
+ * print_list - Prints all the elements of a list_t list.
+ * @h: Pointer to the head of the list.
+ *
+ * Return: The number of nodes in the list.
  */
 size_t print_list(const list_t *h)
 {
