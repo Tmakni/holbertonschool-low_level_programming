7c9fc6e6df84e75fdd7eb5861abffc808e1bab18
Your Name <you@example.com>
2024-12-05 09:58:49 +0000

    ok

--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -3,6 +3,13 @@
 #include <stdio.h>
 #include "lists.h"
 
+/**
+ * list_len - Compte le nombre d'éléments dans une liste chaînée
+ * @h: Pointeur vers le premier nœud de la liste
+ *
+ * Return: Nombre de nœuds dans la liste
+ */
+
 size_t list_len(const list_t *h)
 {
 	size_t count = 0;
@@ -13,28 +20,4 @@ size_t list_len(const list_t *h)
 		h = h->next;
 	}
 	return (count);
-}
-int main(void)
-{
-    list_t *head;
-    list_t *new;
-    list_t hello = {"World", 5, NULL};
-    size_t n;
-
-    head = &hello;
-    new = malloc(sizeof(list_t));
-    if (new == NULL)
-    {
-        printf("Error\n");
-        return (1);
-    }
-    new->str = strdup("Hello");
-    new->len = 5;
-    new->next = head;
-    head = new;
-    n = list_len(head);
-    printf("-> %lu elements\n", n);
-    free(new->str);
-    free(new);
-    return (0);
 }
